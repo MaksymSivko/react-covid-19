361ecf10b81ee6eaa24dd9a6739ff6db8ee973bf
Maksym <ms@facility.team>
2020-05-19 13:43:54 +0300

    add cards

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import React, { Component } from 'react';
 
+import { Cards, Chart, CountryPicker } from './components';
+
+import { fetchData } from './api';
+
+import styles from './App.module.css';
+
 export class App extends Component {
+  state = {
+    data: {},
+  };
+
+  async componentDidMount() {
+    const fetchedDate = await fetchData();
+    this.setState({ data: fetchedDate });
+  }
   render() {
-    return <div>Hello</div>;
+    const { data } = this.state;
+
+    return (
+      <div className={styles.container}>
+        <Cards date={data} />
+        {/* <CountryPicker />
+        <Chart /> */}
+      </div>
+    );
   }
 }
